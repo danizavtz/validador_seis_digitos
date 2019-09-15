67319261747ab5b4fcd05c3a5b1ea8b43824a30f
danizavtz <daniellucena@yahoo.com.br>
2019-09-15 04:50:01 -0300

    Troca aspas simples para duplas

--- tests/test_cep_valido.py
+++ tests/test_cep_valido.py
@@ -4,25 +4,25 @@ from main import verifica_cep_valido
 
 class TestCepValido(unittest.TestCase):
     def test_cep_valido(self):
-        self.assertTrue(verifica_cep_valido("123456"))
+        self.assertTrue(verifica_cep_valido('123456'))
     
     def test_cep_valido_2(self):
-        self.assertTrue(verifica_cep_valido("123455"))
+        self.assertTrue(verifica_cep_valido('123455'))
     
     def test_cep_invalido_por_tamanho_da_cadeia_menor(self):
-        self.assertFalse(verifica_cep_valido("12345"))
+        self.assertFalse(verifica_cep_valido('12345'))
     
     def test_cep_invalido_com_letras(self):
-        self.assertFalse(verifica_cep_valido("123a45"))
+        self.assertFalse(verifica_cep_valido('123a45'))
     
     def test_cep_invalido_com_caracteres_especiais(self):
-        self.assertFalse(verifica_cep_valido("12345!"))
+        self.assertFalse(verifica_cep_valido('12345!'))
     
     def test_cep_invalido_por_tamanho_da_cadeia_maior(self):
-        self.assertFalse(verifica_cep_valido("1234567"))
+        self.assertFalse(verifica_cep_valido('1234567'))
 
     def test_cep_valido_3(self):
-        self.assertTrue(verifica_cep_valido("998877"))
+        self.assertTrue(verifica_cep_valido('998877'))
 
     def test_cep_valido_parametro_tipo_integer(self):
         self.assertTrue(verifica_cep_valido(123456))
