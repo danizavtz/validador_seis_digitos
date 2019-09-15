08211e07d86e649e53f8d770675e3d8106b632e2
danizavtz <daniellucena@yahoo.com.br>
2019-09-14 23:13:28 -0300

    Refactor e adaptacao para principio da responsabilidade unica

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import re
-expressao_regular = '^[1-9](\d+){5}$'
+expressao_regular = '^[1-9][\d+]{5}$'
 
 def verifica_alternado_em_par(valor_a_verificar):
     for num, f in enumerate(valor_a_verificar):
         j = num + 2
         if j < len(valor_a_verificar) and f == valor_a_verificar[j]:
             return True
+    return False
+
+def verifica_cep_valido(cep):
+    resultado = re.match(expressao_regular, cep)
+    if resultado and not verifica_alternado_em_par(cep):
+        return True
     return False
---
+++ tests/test_cep_valido.py
@@ -0,0 +1,28 @@
+import unittest
+from main import verifica_cep_valido
+
+
+class TestCepValido(unittest.TestCase):
+    def test_cep_valido(self):
+        self.assertTrue(verifica_cep_valido("123456"))
+    
+    def test_cep_valido_2(self):
+        self.assertTrue(verifica_cep_valido("123455"))
+    
+    def test_cep_invalido_por_tamanho_da_cadeia_menor(self):
+        self.assertFalse(verifica_cep_valido("12345"))
+    
+    def test_cep_invalido_com_letras(self):
+        self.assertFalse(verifica_cep_valido("123a45"))
+    
+    def test_cep_invalido_com_caracteres_especiais(self):
+        self.assertFalse(verifica_cep_valido("12345!"))
+    
+    def test_cep_invalido_por_tamanho_da_cadeia_maior(self):
+        self.assertFalse(verifica_cep_valido("1234567"))
+
+    def test_cep_valido_3(self):
+        self.assertTrue(verifica_cep_valido("998877"))
+
+if __name__ == '__main__':
+    unittest.main()
